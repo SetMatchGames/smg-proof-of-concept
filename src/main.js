20ba48a2ab983b4c16e22559d0f76cd057711ba9
Jesse B. Miller <jesse@jessebmiller.com>
2018-12-29 13:38:27 -0600

    game finds a winner

--- src/main.js
+++ src/main.js
@@ -6,7 +6,10 @@ const components = JSON.parse(fs.readFileSync('components.json', 'utf8'))
 
 const q = readlineSync.question
 
-const format = q(`Choose a format [${Object.keys(formats["Ro-sham-bo"])}] `)
+let format = q(`Choose a format [${Object.keys(formats["Ro-sham-bo"])}] `)
+if (format === "") {
+  format = "standard"
+}
 const gameComponents = formats["Ro-sham-bo"][format]
 
 const getRules = (answer) => {
@@ -17,13 +20,46 @@ const getRules = (answer) => {
 
 }
 
+const findWinner = (p1, p2) => {
+  let p1Wins = false
+  let p2Wins = false
+
+  // Check all the options of which choice wins and loses against the other
+  // choice
+  if (p1.rules.winsAgainst.includes(p2.choice)) {
+    p1Wins = true
+  }
+  if (p2.rules.winsAgainst.includes(p1.choice)) {
+    p2Wins = true
+  }
+  if (p1.rules.losesAgainst.includes(p2.choice)) {
+    p2Wins = true
+  }
+  if (p2.rules.losesAgainst.includes(p1.choice)) {
+    p1Wins = true
+  }
+
+  // if there is a unique winner return that winner
+
+  if (p1Wins === p2Wins) {
+    return false
+  }
+
+  if (p1Wins) {
+    return 1
+  }
+
+  return 2
+}
+
 let p1, p2
 let winner = false
 while (!winner) {
+  console.log("No winner yet...")
   p1 = getRules(q(`Player one choose a component from [${gameComponents}]: `))
   p2 = getRules(q(`Player two choose a component from [${gameComponents}]: `))
 
-  console.log("P1", p1)
-  console.log("P2", p2)
-  winner = true
+  winner = findWinner(p1, p2)
 }
+
+console.log("Plyer", winner, "Wins.")
